9a04a135dec209c3ea747d5215961eacdcbe0d81
Jim_Sun <jimsun6428@gmail.com>
2023-03-02 06:41:18 +0000

    pageContent layout

---
+++ components/Layout/PageContent.tsx
@@ -0,0 +1,36 @@
+import { Flex } from "@chakra-ui/react";
+import React from "react";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export default function PageContent({ children }: Props) {
+  return (
+    <Flex justify="center" padding="16px 0px" border="1px solid red">
+      <Flex
+        width="95%"
+        maxWidth="860px"
+        justify="center"
+        border="1px solid blue"
+      >
+        {/* Left */}
+        <Flex
+          width={{ base: "100%", md: "65%" }}
+          mr={{ base: 0, md: 6 }}
+          border="1px solid green"
+        >
+          {children && children[0 as keyof typeof children]}
+        </Flex>
+        {/* Right */}
+        <Flex
+          display={{ base: "none", md: "flex" }}
+          flexGlow={1}
+          border="1px solid orange"
+        >
+          {children && children[1 as keyof typeof children]}
+        </Flex>
+      </Flex>
+    </Flex>
+  );
+}
