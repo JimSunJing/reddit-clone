6e02620886dd2ec2db870c527529ee53efe62913
JimS <jimsun6428@gmail.com>
2023-03-17 17:53:37 +0800

    submit page About

--- pages/r/[communityId]/submit.tsx
+++ pages/r/[communityId]/submit.tsx
@@ -1,7 +1,9 @@
 import { CommunityStateAtom } from "@/atoms/communitiesAtom";
+import About from "@/components/Community/About";
 import PageContent from "@/components/Layout/PageContent";
 import NewPostForm from "@/components/Posts/NewPostForm";
 import { auth } from "@/firebase/clientApp";
+import useCommunityData from "@/hooks/useCommunityHook";
 import { Box, Text } from "@chakra-ui/react";
 import React from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
@@ -9,7 +11,8 @@ import { useRecoilValue } from "recoil";
 
 export default function Submit() {
   const [user] = useAuthState(auth);
-  const communityStateValue = useRecoilValue(CommunityStateAtom);
+  // const communityStateValue = useRecoilValue(CommunityStateAtom);
+  const { communityStateValue } = useCommunityData();
   return (
     <PageContent>
       <>
@@ -18,7 +21,11 @@ export default function Submit() {
         </Box>
         {user && <NewPostForm user={user} />}
       </>
-      <>{/* <About /> */}</>
+      <>
+        {communityStateValue.currentCommunity && (
+          <About communityData={communityStateValue.currentCommunity} />
+        )}
+      </>
     </PageContent>
   );
 }
