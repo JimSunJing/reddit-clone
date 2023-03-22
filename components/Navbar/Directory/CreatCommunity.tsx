10b6f4703c3fa69603d7f17721bf0263c6eabe74
JimS <jimsun6428@gmail.com>
2023-03-22 16:03:51 +0800

    community list

--- components/Navbar/Directory/CreatCommunity.tsx
+++ components/Navbar/Directory/CreatCommunity.tsx
@@ -1,26 +1,46 @@
+import { CommunityStateAtom } from "@/atoms/communitiesAtom";
 import CreateCommunityModal from "@/components/Modal/CreateCommunity/CreateCommunityModal";
-import { Flex, Icon, MenuItem } from "@chakra-ui/react";
+import { Box, Flex, Icon, MenuItem, Text } from "@chakra-ui/react";
 import React, { useState } from "react";
+import { FaReddit } from "react-icons/fa";
 import { GrAdd } from "react-icons/gr";
+import { useRecoilValue } from "recoil";
+import MenuListItem from "./MenuListItem";
 
 // type Props = {};
 
 export default function CreateCommunity() {
   const [open, setOpen] = useState(false);
+  const mySnippets = useRecoilValue(CommunityStateAtom).mySnippets;
   return (
     <>
       <CreateCommunityModal open={open} setClose={() => setOpen(false)} />
-      <MenuItem
-        _hover={{ bg: "gray.100" }}
-        fontSize="10pt"
-        width="100%"
-        onClick={() => setOpen(true)}
-      >
-        <Flex align="center">
-          <Icon as={GrAdd} fontSize={20} mr={2} />
-          Create Community
-        </Flex>
-      </MenuItem>
+      <Box mt={3} mb={4}>
+        <Text pl={3} mb={1} fontSize="7pt" fontWeight={500} color="gray.500">
+          MY COMMUNITIES
+        </Text>
+        <MenuItem
+          _hover={{ bg: "gray.100" }}
+          fontSize="10pt"
+          width="100%"
+          onClick={() => setOpen(true)}
+        >
+          <Flex align="center">
+            <Icon as={GrAdd} fontSize={20} mr={2} />
+            Create Community
+          </Flex>
+        </MenuItem>
+        {mySnippets.map((snippet) => (
+          <MenuListItem
+            key={snippet.communityId}
+            displayText={snippet.communityId}
+            link={`/r/${snippet.communityId}`}
+            icon={FaReddit}
+            iconColor="blue.500"
+            imageURL={snippet.imageURL}
+          />
+        ))}
+      </Box>
     </>
   );
 }
